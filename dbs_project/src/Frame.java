2d86574f26b3eb318401c767d5cfab4409755f97
pranavv15 <vinodpranav15@gmail.com>
2021-12-03 16:05:40 -0500

    Working on Frames

--- dbs_project/src/Frame.java
+++ dbs_project/src/Frame.java
@@ -1,8 +1,50 @@
 import java.awt.*; // for Dimension
 import javax.swing.*; // for GUI components
-// import App.java;
+import java.awt.event.*; // for action events
+
+// public class MessageListener implements ActionListener {
+//     public void actionPerformed(ActionEvent event) {
+//         JOptionPane.showMessageDialog(null,
+//                          "You clicked the button!");
+//              }
+
+//             //  public class Frame {
+//             //     public static void main(String[] args) {
+                
+//             //                 JFrame frame = new JFrame();
+//             //                 frame.setForeground(Color.WHITE);
+//             //                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//             //                 frame.setLocation(new Point(550, 200));
+//             //                 frame.setSize(new Dimension(600, 400));
+//             //                 frame.setTitle("A frame");
+            
+//             //                 JButton button1 = new JButton();
+//             //                 button1.setBackground(Color.YELLOW);
+//             //                 button1.setText("This is a button");
+//             //                 frame.add(button1);
+            
+//             //                 JButton button2 = new JButton();
+//             //                 button2.setBackground(Color.red);
+//                         //     button2.setText("This is a button");
+//                         //     button2.addActionListener(new MessageListener());
+//                         //     frame.add(button2);
+            
+//                         //     frame.setLayout(new FlowLayout());
+//                         //     frame.pack();
+            
+//                         //     frame.setVisible(true);
+//                         //  }
+//             }
+//          }
+
+
 public class Frame {
-    // import javax.swing.*; // for GUI components
+    static class MessageListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            JOptionPane.showMessageDialog(null,
+                             "You clicked the button!");
+                }
+            }
     public static void main(String[] args) {
     
                 JFrame frame = new JFrame();
@@ -20,6 +62,7 @@ public class Frame {
                 JButton button2 = new JButton();
                 button2.setBackground(Color.red);
                 button2.setText("This is a button");
+                button2.addActionListener(new MessageListener());
                 frame.add(button2);
 
                 frame.setLayout(new FlowLayout());
